f26f46d7efbaf1c6245ac7612a2b020487aaf246
dakotahm <dakotah.moore523@gmail.com>
2017-04-11 11:18:47 -0400

    functionality added

---
+++ RecordEvent/static/scripts/RecordEntryScript.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Dakotah on 4/9/2017.
+ */
+$("document").ready(function() {
+    $("#DatePicker").datepicker();
+});
+
+
+$( function() {
+    $( "#TimePicker" ).timepicker();
+  } );
