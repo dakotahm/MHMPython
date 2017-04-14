1e5d03c89780da3bc3ce7d06ca4d1809ce17cd97
DESKTOP-SOUFIN\Soufin Rahimeen <soufinrahimeen@gmail.com>
2017-04-14 16:59:01 -0400

    setting up api end points

--- DisplayData/urls.py
+++ DisplayData/urls.py
@@ -1,6 +1,12 @@
 from django.conf.urls import url
+from django.contrib import admin
 from . import views
+from .views import ChartData
+
+#from .views import get_data, ChartData
 
 urlpatterns=[
-    url(r'^$',views.DisplayView, name='DisplayView')
+    url(r'^$',views.DisplayView, name='DisplayView'),
+    url(r'^api/data/$', views.get_data, name='api-data'),
+    url(r'^api/chart/data/$', views.ChartData.as_view())
 ]
--- DisplayData/views.py
+++ DisplayData/views.py
@@ -1,29 +1,33 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
-from django.contrib.auth.models import User
+from django.http import JsonResponse
 
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-class AnalyticsIndexView(TemplateView):
-    template_name = 'analytics/admin/index.html'
-
-    def get_context_data(self, **kwargs):
-        context = super(AnalyticsIndexView, self).get_context_data(**kwargs)
-        context['30_day_registrations'] = self.thirty_day_registrations()
-        return context
-
-    def thirty_day_registrations(self):
-        final_data = []
-
-        date = arrow.now()
-        for day in xrange(1, 30):
-            date = date.replace(days=-1)
-            count = User.objects.filter(
-                date_joined__gte=date.floor('day').datetime,
-                date_joined__lte=date.ceil('day').datetime).count()
-            final_data.append(count)
-
-        return final_data
 
 def DisplayView(request):
     return render(request,'DisplayData/Display.html')
+
+
+def get_data(request, *args, **kwargs):
+	data = {
+		"sales": 100,
+		"customers": 10,
+	}
+	return JsonResponse(data)
+
+class ChartData(APIView):
+	authentication_classes = []
+	permission_classes = []
+
+	def get(self, request, format=None):
+		data = {
+			"sales": 100,
+			"customers": 10,
+		}	
+		return Response(data)
+
+
+
+
 
