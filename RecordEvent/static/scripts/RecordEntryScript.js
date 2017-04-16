3b2e1586617e5149c97d717697a1ceafd6369b2d
dakotahm <dakotah.moore523@gmail.com>
2017-04-15 20:52:55 -0400

    Lots of JS work

--- RecordEvent/static/scripts/RecordEntryScript.js
+++ RecordEvent/static/scripts/RecordEntryScript.js
@@ -6,6 +6,53 @@ $("document").ready(function() {
 });
 
 
-$( function() {
-    $( "#TimePicker" ).timepicker();
-  } );
+// $( function() {
+//     $( "#TimePicker" ).timepicker();
+//   } );
+
+$("document").ready(function() {
+    $('.dropdown-menu li').on('click', function() {
+    $('#dropdownTitle').html($(this).find('a').html());
+    document.getElementById("measurable").value=$(this).attr("class");
+    $("#id_valueText").attr("min",$(this).attr("min"));
+    $("#id_valueText").attr("max",$(this).attr("max"));
+    console.log($(this).attr("max"));
+    console.log($(this).attr("min"));
+
+    if($(this).attr("max")=="None" && $(this).attr("min")=="None")
+        $("#id_valueText").attr('disabled','disabled')
+    else
+        $("#id_valueText").removeAttr('disabled');
+
+    // console.log($("#measurable").attr("max"));
+    });
+});
+
+$("document").ready(function() {
+    $('.dropdown-menu li').on('click', function() {
+    $('#dropdownTitle').html($(this).find('a').html());
+    document.getElementById("measurable").value=$(this).attr("class");
+    $("#id_valueText").attr("min",$(this).attr("min"));
+    $("#id_valueText").attr("max",$(this).attr("max"));
+    console.log($(this).attr("max"));
+    console.log($(this).attr("min"));
+
+    if($(this).attr("max")=="None" && $(this).attr("min")=="None")
+        $("#id_valueText").attr('disabled','disabled')
+    else
+        $("#id_valueText").removeAttr('disabled');
+
+    // console.log($("#measurable").attr("max"));
+    });
+});
+
+$("document").ready(function() {
+    function Validate() {
+        if ($('#measurable').attr("class") == -1) {
+            alert("Please select a valid measurable");
+            return false;
+        }
+        console.log("Success")
+        return true;
+    }
+});
--- RecordEvent/views.py
+++ RecordEvent/views.py
@@ -1,20 +1,30 @@
 from django.shortcuts import render
 from RecordEvent import  models
+from datetime import datetime,date
 
 def index(request):
     from .forms import LogForm
+    #hardcoded user1 for testing
+    Measurables=[ entry for entry in models.Measurables.objects.all().filter(user_id=1)]
+
     if (request.method=='POST'):
 
         form=LogForm(request.POST)
         # print(request)
         # print(request.POST)
         # print()
+
         test=models.Entries.objects.all()
         print(test)
-
+        print(request.POST)
         if form.is_valid():
             print(request.POST)
-            print(request.POST.get('value',-1))
+            print(request.POST.get('date',datetime.today().strftime('%Y-%m-%d')))
+            print(request.POST.get('time', datetime.today().strftime('%H:%M:%S')))
+            print(request.POST.get('log', ''))
+            print(request.POST.get('location', ''))
+            print(request.POST.get('value', -1))
+            print(request.POST.get('measureId', '-1'))
 
 
 
@@ -24,4 +34,6 @@ def index(request):
     else:
         form=LogForm()
         print("blank form")
-    return render(request,'RecordEvent/Record.html')
+        #test = int('fail')
+
+    return render(request,'RecordEvent/Record.html',{'dropdown':Measurables})
