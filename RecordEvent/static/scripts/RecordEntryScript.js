01f09165604d1f891b07184810bc1e4293fb4eb5
dakotahm <dakotah.moore523@gmail.com>
2017-04-16 23:48:58 -0400

    more form work

--- RecordEvent/static/scripts/RecordEntryScript.js
+++ RecordEvent/static/scripts/RecordEntryScript.js
@@ -14,39 +14,41 @@ $("document").ready(function() {
     $('.dropdown-menu li').on('click', function() {
     $('#dropdownTitle').html($(this).find('a').html());
     document.getElementById("measurable").value=$(this).attr("class");
+    console.log($(this));
     $("#id_valueText").attr("min",$(this).attr("min"));
     $("#id_valueText").attr("max",$(this).attr("max"));
     console.log($(this).attr("max"));
     console.log($(this).attr("min"));
 
-    if($(this).attr("max")=="None" && $(this).attr("min")=="None")
-        $("#id_valueText").attr('disabled','disabled')
-    else
-        $("#id_valueText").removeAttr('disabled');
 
-    $("#submitDisabled").removeAttr('disabled');
-    $("#submitDisabled").attr('class','pull-right')
-    // console.log($("#measurable").attr("max"));
+
+try{
+     if (document.getElementById("measurable").value != -1 && $('#id_valueText').val().length>0) {
+           //console.log("Enter the value of asindapinsd");
+           $("#submitDisabled").removeAttr('disabled');
+           $("#submitDisabled").attr('class','pull-right');
+     }else{
+           $("#submitDisabled").attr('disabled','disabled');
+           $("#submitDisabled").attr('class','pull-right disabled')
+
+     }
+}catch(ex) {
+
+}
+
+
+     if($(this).attr("max")=="None" && $(this).attr("min")=="None"){
+        $("#id_valueText").attr('disabled','disabled');
+        $("#submitDisabled").removeAttr('disabled');
+        $("#submitDisabled").attr('class','pull-right');
+        console.log(document.getElementById("id_valueText").value)
+    } else{
+        $("#id_valueText").removeAttr('disabled');
+        document.getElementById("id_valueText").value="";
+    }
     });
 });
 
-// $("document").ready(function() {
-//     $('.dropdown-menu li').on('click', function() {
-//     $('#dropdownTitle').html($(this).find('a').html());
-//     document.getElementById("measurable").value=$(this).attr("class");
-//     $("#id_valueText").attr("min",$(this).attr("min"));
-//     $("#id_valueText").attr("max",$(this).attr("max"));
-//     console.log($(this).attr("max"));
-//     console.log($(this).attr("min"));
-//
-//     if($(this).attr("max")=="None" && $(this).attr("min")=="None")
-//         $("#id_valueText").attr('disabled','disabled')
-//     else
-//         $("#id_valueText").removeAttr('disabled');
-//         $("#submitDisabled").removeAttr('disabled');
-//     // console.log($("#measurable").attr("max"));
-//     });
-// });
 
 $("document").ready(function() {
     function Validate() {
@@ -61,5 +63,37 @@ $("document").ready(function() {
 
 
 $("document").ready(function () {
+$('#id_valueText').on('input', function() {
+     if (document.getElementById("measurable").value != -1 && $('#id_valueText').val().length>0) {
+           //console.log("Enter the value of asindapinsd");
+           $("#submitDisabled").removeAttr('disabled');
+           $("#submitDisabled").attr('class','pull-right');
+     }else{
+           $("#submitDisabled").attr('disabled','disabled');
+           $("#submitDisabled").attr('class','pull-right disabled')
 
+     }
+       // console.log( document.getElementById("measurable").value);
+});
+});
+
+$("document").ready(function () {
+$('#id_NameText').on('input', function() {
+     if (document.getElementById("measurable").value != -1 && $('#id_NameText').val().length>0) {
+           //console.log("Enter the value of asindapinsd");
+           $("#submitDisabled").removeAttr('disabled');
+           $("#submitDisabled").attr('class','pull-right');
+     }else{
+           $("#submitDisabled").attr('disabled','disabled');
+           $("#submitDisabled").attr('class','pull-right disabled')
+
+     }
+       // console.log( document.getElementById("measurable").value);
+});
+});
+
+("document").ready(function() {
+    $('.dropdown-menu li').on('click', function () {
+        console.log("hey")
+    });
 });
--- RecordEvent/views.py
+++ RecordEvent/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from RecordEvent import  models
 from datetime import datetime,date
+import json
 
 def index(request):
     from .forms import LogForm
@@ -11,7 +12,7 @@ def index(request):
 
         form=LogForm(request.POST)
         # print(request)
-        # print(request.POST)
+        print(request.POST)
         # print()
 
         test=models.Entries.objects.all()
@@ -19,28 +20,18 @@ def index(request):
         print(request.POST)
         if form.is_valid():
             print(request.POST)
-            dateInput=request.POST.get('date',datetime.today().strftime('%Y-%m-%d'))
-            timeInput=request.POST.get('time', datetime.today().strftime('%H:%M:%S'))
+            dateInput=request.POST.get('date',datetime.today()).split("/")
+            timeInput=request.POST.get('time', datetime.today()).split(':')
+            finalDatetime= datetime(int(dateInput[2]),int(dateInput[0]),int(dateInput[1]),int(timeInput[0]),int(timeInput[1]))
             log=request.POST.get('log', '')
             location=request.POST.get('location', '')
             value=request.POST.get('value', -1)
             measureID=request.POST.get('measureId', '-1')
 
-            # if dateInput.isspace() and timeInput.isspace():
-            #     theTime=datetime.now()
-            # elif not dateInput.isspace() and not timeInput.isspace():
-            #     theTime=datetime.strptime(dateInput+' '+timeInput, "%m/%d/%Y %I:%M:%S %p")
-            # elif not dateInput.isspace() and  timeInput.isspace():
-            #     theTime=datetime.strptime(dateInput+' '+timeInput, "%m/%d/%Y")
-            # elif dateInput.isspace() and not timeInput.isspace():
-            #     theTime=datetime.strptime(timeInput, "%I:%M:%S %p")
-           # newEntry = models.Entries(parent=measureID,timestamp=theTime,value=)
+            toJson={'log':log,'value':value,'location':location}
+            newEntry = models.Entries(parent=measureID,timestamp=finalDatetime,data=json.dumps(toJson))
 
-        # class Entries(models.Model):
-        #     id = models.BigAutoField(primary_key=True)
-        #     parent = models.BigIntegerField()
-        #     data = models.CharField(max_length=20000)
-        #     timestamp = models.DateTimeField(blank=True, null=True)
+
 
         else:
             print("Form not valid")
@@ -53,4 +44,10 @@ def index(request):
     return render(request,'RecordEvent/Record.html',{'dropdown':Measurables})
 
 def insertMeasurable(request):
+    if (request.method == 'POST'):
+        print(request.POST)
+        minInput=request.POST.get('min',0);
+        maxInput=request.POST.get('max',100)
+        nameInput=request.POST.get('name','name')
+        newMeasurable=models.Measurables(name=nameInput,max=int(maxInput),min=int(minInput),user_id=1)
     return render(request,'RecordEvent/AddMeasurable.html')
