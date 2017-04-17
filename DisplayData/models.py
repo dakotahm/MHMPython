dd35388db3c1cd9080f3e4b29bd21f055cd8b831
DESKTOP-SOUFIN\Soufin Rahimeen <soufinrahimeen@gmail.com>
2017-04-17 01:42:58 -0400

    chart now uses rest api to get data and generates

--- DisplayData/models.py
+++ DisplayData/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Entries(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    parent = models.BigIntegerField()
+    data = models.CharField(max_length=20000)
+    timestamp = models.DateTimeField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'entries'
--- DisplayData/views.py
+++ DisplayData/views.py
@@ -23,51 +23,13 @@ class ChartData(APIView):
 	permission_classes = []
 
 	def get(self, request, format=None):
+		labels = ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"]
+		default_items = [12, 11, 17, 12, 12, 2]
 		data = {
-			"sales": 100,
-			"customers": 10,
+			"labels": labels,
+			"default": default_items,
 		}	
 		return Response(data)
-
-def weather_chart_view(request):
-    #Step 1: Create a DataPool with the data we want to retrieve.
-    weatherdata = \
-        DataPool(
-           series=
-            [{'options': {
-               'source': Measurables.objects.all()},
-              'terms': [
-                'name',
-                'type',
-                'max',
-                'min']}
-             ])
-
-    #Step 2: Create the Chart object
-    cht = Chart(
-            datasource = weatherdata,
-            series_options =
-              [{'options':{
-                  'type': 'line',
-                  'stacking': False},
-                'terms':{
-                  'month': [
-                    'name',
-                    'type',
-                    'max',
-                    'min']
-                  }}],
-            chart_options =
-              {'title': {
-                   'text': 'Weather Data of Boston and Houston'},
-               'xAxis': {
-                    'title': {
-                       'text': 'Month number'}}})
-
-    #Step 3: Send the chart object to the template.
-    return render_to_response({'weatherchart': cht})
-
-
 
 
 
