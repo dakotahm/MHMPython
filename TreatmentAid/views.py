a035466988ae819f02c1d0898b6f2dfba86612cb
JFerrGit <johnrffacebookemail@gmail.com>
2017-04-18 13:19:34 -0400

    Update views.py

--- TreatmentAid/views.py
+++ TreatmentAid/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.contrib import auth
+from TreatmentAid import  models
+from django.db.models import Q
+
 
 @login_required
 def AidView(request):
-    return render(request,'TreatmentAid/Aid.html')
+
+    # code for current userid
+    current_user = request.user
+    print(current_user)
+    print(current_user.id)
+
+    Downloadables = [down for down in models.Treatmentdownloads.objects.all().filter(Q(userfk=current_user.id) | Q(global_field=1))]
+
+    return render(request,'TreatmentAid/Aid.html',{'download':Downloadables})
