1a0d5f2557524d0471e6c5ad11608f08f256d670
DESKTOP-SOUFIN\Soufin Rahimeen <soufinrahimeen@gmail.com>
2017-04-19 23:13:22 -0400

    graph working

--- DisplayData/views.py
+++ DisplayData/views.py
@@ -8,6 +8,8 @@ from django.contrib.auth.decorators import login_required
 
 from chartit import DataPool, Chart
 
+import json
+
 
 @login_required
 def DisplayView(request):
@@ -26,21 +28,29 @@ class ChartData(APIView):
 	permission_classes = []
 
 	def get(self, request, format=None):
-		all_entries = models.Entries.objects.all().filter(id=2)
+		all_entries = models.Entries.objects.all().filter(parent=2)
 		all_id = models.Entries.objects.all().values_list('id', flat=True)
 		all_measurables = models.Measurables.objects.all().filter(user_id=1)
 		
 		all_ids = [m.id for m in all_measurables]
 		all_times = [m.timestamp for m in all_entries]
-		all_data = [m.data for m in all_entries]
+
+		all_data = []
+		for m in all_entries:
+			data = m.data
+			json_data = json.loads(data)
+			value = json_data['value']
+			all_data.append(value)
+
+		#all_data = [m.data for m in all_entries]
 
 
 		all_parent = models.Entries.objects.all().values_list('parent', flat=True)
 		labels = ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"]
 		default_items = [12, 11, 17, 12, 12, 2]
 		data = {
-			"labels": all_data,
-			"default": all_times,
+			"labels": all_times,
+			"default": all_data,
 			#"default": all_parent,
 		}	
 		return Response(data)
