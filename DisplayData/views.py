37c7c2b9e28076066850947257bd510ee2eb3967
DESKTOP-SOUFIN\Soufin Rahimeen <soufinrahimeen@gmail.com>
2017-04-19 23:03:56 -0400

    queries working

--- DisplayData/views.py
+++ DisplayData/views.py
@@ -26,15 +26,21 @@ class ChartData(APIView):
 	permission_classes = []
 
 	def get(self, request, format=None):
-		all_entries = models.Entries.objects.all()
+		all_entries = models.Entries.objects.all().filter(id=2)
 		all_id = models.Entries.objects.all().values_list('id', flat=True)
-		all_measurables = models.Measurables.objects.all().filter(user_id=request.user.id)
+		all_measurables = models.Measurables.objects.all().filter(user_id=1)
+		
+		all_ids = [m.id for m in all_measurables]
+		all_times = [m.timestamp for m in all_entries]
+		all_data = [m.data for m in all_entries]
+
+
 		all_parent = models.Entries.objects.all().values_list('parent', flat=True)
 		labels = ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"]
 		default_items = [12, 11, 17, 12, 12, 2]
 		data = {
-			"labels": all_measurables,
-			#"labels": all_id,
+			"labels": all_data,
+			"default": all_times,
 			#"default": all_parent,
 		}	
 		return Response(data)
