ac4ba91e638a5a0aa21db9fb0ee3e655f9ef8480
JFerrGit <johnrffacebookemail@gmail.com>
2017-04-21 13:20:02 -0400

    Update views.py

--- TreatmentAid/views.py
+++ TreatmentAid/views.py
@@ -4,6 +4,7 @@ from django.contrib import auth
 from TreatmentAid import  models
 from django.db.models import Q
 import json
+from django.http import HttpResponse
 
 
 @login_required
@@ -16,10 +17,15 @@ def AidView(request):
 
     Downloadables = [down for down in models.Treatmentdownloads.objects.all().filter(Q(userfk=current_user.id) | Q(global_field=1))]
 
-    if(request.method=='GET' and request.is_ajax()):
+    #if(request.method=='GET' and request.is_ajax()):
         #puts all the get parameters in a list
         #for some reason the key 'url' was not working
-        theURL=[request.GET[key] for key in request.GET][0]
+        #theURL=[request.GET[key] for key in request.GET][0]
+
+        #response = HttpResponse("README.txt")
+        #response['Content-Type'] = 'text/plain'
+        #response['Content-Disposition'] = 'attachment; filename=README.txt'
+        #return response
 
 
     return render(request,'TreatmentAid/Aid.html',{'download':Downloadables})
