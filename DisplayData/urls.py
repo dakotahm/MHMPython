374871eb8ad9742f24fa217ff29b7d71df2df234
DESKTOP-SOUFIN\Soufin Rahimeen <soufinrahimeen@gmail.com>
2017-04-25 02:04:10 -0400

    added text form

--- DisplayData/urls.py
+++ DisplayData/urls.py
@@ -4,11 +4,13 @@ from . import views
 
 
 
-from .views import get_data, ChartData
+from .views import get_data, ChartData, search, index
 
 urlpatterns=[
     url(r'^$',views.DisplayView, name='DisplayView'),
     url(r'^api/data/$', views.get_data, name='api-data'),
     url(r'^display/api/chart/data/$', views.ChartData.as_view()),
-    url(r'^logs/', views.LogDisplay, name='Display-Logs')
+    url(r'^logs/', views.LogDisplay, name='Display-Logs'),
+    url(r'^search/', search),
+    url(r'^index/', index)
 ]
--- DisplayData/views.py
+++ DisplayData/views.py
@@ -16,42 +16,61 @@ from RecordEvent.forms import LogForm,ValidateRecordEvent,ValidateNewLog
 
 @login_required
 def DisplayView(request):
-	return render(request,'DisplayData/Display.html')
+    return render(request,'DisplayData/Display.html')
 
 
 def get_data(request, *args, **kwargs):
-	data = {
-		"sales": 100,
-		"customers": 10,
-	}
-	return JsonResponse(data)
+    data = {
+        "sales": 100,
+        "customers": 10,
+    }
+    return JsonResponse(data)
+
+
+
+def index(request):
+    return render(request, 'form.html')
+
+display_index = 0
+
+def search(request):
+    if request.method == 'POST':
+        search_id = request.POST['textfield']
+        print(search_id)
+        display_index = search_id
+    else:
+        return render(request, 'form.html')   
+
 
 class ChartData(APIView):
-	authentication_classes = []
-	permission_classes = []
+    authentication_classes = []
+    permission_classes = []
 
-	def get(self, request, format=None):
-		all_entries = models.Entries.objects.all().filter(parent=2) #change to input from drop down
-		all_id = models.Entries.objects.all().values_list('id', flat=True)
-		all_measurables = models.Measurables.objects.all().filter(user_id=1) #change to current user
-		
-		all_ids = [m.id for m in all_measurables] #use this to make drop down
-		all_times = [m.timestamp for m in all_entries]
 
-		all_data = []
-		for m in all_entries:
-			data = m.data
-			json_data = json.loads(data)
-			value = json_data['value']
-			all_data.append(value)
 
-		
-		data = {
-			"labels": all_times,
-			"default": all_data,
-			#"default": all_parent,
-		}	
-		return Response(data)
+    def get(self, request, format=None):
+
+        all_entries = models.Entries.objects.all().filter(parent=display_index) #change to input from drop down or change to 2
+        all_id = models.Entries.objects.all().values_list('id', flat=True)
+        all_measurables = models.Measurables.objects.all().filter(user_id=request.user.id) #change to current user
+        
+        all_ids = [m.id for m in all_measurables] #use this to make drop down
+        
+        all_times = [m.timestamp for m in all_entries]
+
+        all_data = []
+        for m in all_entries:
+            data = m.data
+            json_data = json.loads(data)
+            value = json_data['value']
+            all_data.append(value)
+
+        
+        data = {
+            "labels": all_times,
+            "default": all_data,
+        }   
+        return Response(data)
 
 @login_required
 def LogDisplay(request):
